3f562ac4da371aba9743e61930504f404f131d51
Swastik Subhasis <swastik.mptra@gmail.com>
2021-11-06 20:16:45 +0530

    sparkOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/havoc_vince.mk
+    $(LOCAL_DIR)/spark_vince.mk
 
 COMMON_LUNCH_CHOICES := \
-    havoc_vince-user \
-    havoc_vince-userdebug \
-    havoc_vince-eng
+    spark_vince-user \
+    spark_vince-userdebug \
+    spark_vince-eng
